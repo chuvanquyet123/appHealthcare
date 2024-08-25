7aa48bfbc2b5b15f02e1c1a4a33b02f7673ddea0
chuvanquyet123 <quyeter29@gmail.com>
2024-08-25 23:06:13 +0700

    choTu

--- navigators/AppNavigator.js
+++ navigators/AppNavigator.js
@@ -2,9 +2,11 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Login from "../screens/LogInPage/loginPage";
-import homePage from "../screens/HomePage/homePage";
-import profilePage from "../screens/ProfilePage/profilePage";
+import {Feather, Entypo} from '@expo/vector-icons';
+import LogIn from "../screens/logInPage/loginPage";
+import homePage from "../screens/homePage/homePage";
+import profilePage from "../screens/profilePage/profilePage";
+import chatPage from "../screens/chatPage/chatPage";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -12,8 +14,11 @@ const Tab = createBottomTabNavigator();
 function MainTabs() {
   return (
     <Tab.Navigator screenOptions={{headerShown: false,}}>
-      <Tab.Screen name="Trang Chủ" component={homePage} options={{tabBarIcon:({}) =>(<Feather name="home" size={25} color={'black'}/>),}}/>
-      <Tab.Screen name="Profile" component={profilePage} />
+      <Tab.Screen name="Trang Chủ" component={homePage} options={{tabBarIcon:({color}) =>(<Feather name="home" size={28} color={color}/>)}}/>
+      <Tab.Screen name="Hội Thoại" component={chatPage} options={{tabBarIcon:({color}) =>(<Entypo name="chat" size={28} color={color}/>)}}/>
+      <Tab.Screen name="Lịch Uống Thuốc" component={homePage} options={{tabBarIcon:({color}) =>(<Feather name="calendar" size={28} color={color}/>)}}/>
+      <Tab.Screen name="Hồ Sơ" component={homePage} options={{tabBarIcon:({color}) =>(<Feather name="folder" size={28} color={color}/>)}}/>
+      <Tab.Screen name="Cá Nhân" component={profilePage} options={{tabBarIcon:({color}) =>(<Feather name="user" size={28} color={color}/>)}}/>
     </Tab.Navigator>
   );
 }
@@ -21,11 +26,11 @@ function MainTabs() {
 export default function AppNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator >
+      <Stack.Navigator>
         <Stack.Screen
           options={{ headerShown: false }}
           name="Login"
-          component={Login}
+          component={LogIn}
         />
         <Stack.Screen
           options={{ headerShown: false }}
