2cef48f930903a18bfbd5ce54ecbd9b1dc893808
Nguyen Tuan Anh <mynametanh12@gmail.com>
2024-08-27 19:27:15 +0700

    hih

--- screens/ReminderPage/reminderPage.js
+++ screens/ReminderPage/reminderPage.js
@@ -5,7 +5,6 @@ import { Ionicons } from '@expo/vector-icons';
 import { useNavigation, useRoute } from "@react-navigation/native";
 import * as Notifications from 'expo-notifications';
 
-
 const ReminderPage = () => {
 
   const [notificationId, setNotificationId] = useState(null);
