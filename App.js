ef42ae14cca0ef45e1995a839347fda40c2237ff
Nguyen Tuan Anh <mynametanh12@gmail.com>
2024-08-27 16:48:16 +0700

    mạng như đb

--- App.js
+++ App.js
@@ -1,6 +1,24 @@
 import React from "react";
 import AppNavigator from "./navigators/AppNavigator";
+import * as Notifications from 'expo-notifications';
 
+// Lắng nghe khi notification được nhận
+useEffect(() => {
+  // Trường hợp foreground
+  const foregroundSubscription = Notifications.addNotificationReceivedListener(() => {
+    navigation.navigate('GiaoDien');
+  });
+
+  // Trường hợp background hoặc khi ứng dụng bị đóng
+  const backgroundSubscription = Notifications.addNotificationResponseReceivedListener(() => {
+    navigation.navigate('GiaoDien');
+  });
+
+  return () => {
+    foregroundSubscription.remove();
+    backgroundSubscription.remove();
+  };
+}, [navigation]);
 export default function App() {
   return <AppNavigator />;
 }
