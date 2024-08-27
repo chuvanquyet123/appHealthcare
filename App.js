d66865dbbec25daf54b144236005f7c8cf9639b2
chuvanquyet123 <quyeter29@gmail.com>
2024-08-27 22:19:59 +0700

    ok

--- App.js
+++ App.js
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AppNavigator from "./navigators/AppNavigator";
-import * as Notifications from 'expo-notifications';
+import * as Notifications from "expo-notifications";
 
 // Lắng nghe khi notification được nhận
 useEffect(() => {
   // Trường hợp foreground
-  const foregroundSubscription = Notifications.addNotificationReceivedListener(() => {
-    navigation.navigate('GiaoDien');
-  });
+  const foregroundSubscription = Notifications.addNotificationReceivedListener(
+    () => {
+      navigation.navigate("GiaoDien");
+    }
+  );
 
   // Trường hợp background hoặc khi ứng dụng bị đóng
-  const backgroundSubscription = Notifications.addNotificationResponseReceivedListener(() => {
-    navigation.navigate('GiaoDien');
-  });
+  const backgroundSubscription =
+    Notifications.addNotificationResponseReceivedListener(() => {
+      navigation.navigate("GiaoDien");
+    });
 
   return () => {
     foregroundSubscription.remove();
