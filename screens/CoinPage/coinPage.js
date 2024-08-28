9dae329fa5e654d9e20946e011a4ef5a62f86be5
chuvanquyet123 <quyeter29@gmail.com>
2024-08-28 19:27:38 +0700

    ok

--- screens/CoinPage/coinPage.js
+++ screens/CoinPage/coinPage.js
@@ -78,7 +78,7 @@ export default function Coin() {
                     652
                   </Text>
                 ) : (
-                  <Text>*******</Text>
+                  <Text>****</Text>
                 )}
                 <TouchableOpacity onPress={handleShowCoin}>
                   {showCoin ? (
--- screens/ReferFriendsPage/referFriendsPage.js
+++ screens/ReferFriendsPage/referFriendsPage.js
@@ -38,162 +38,166 @@ export default function ReferFriends() {
           Mời Bạn
         </Text>
       </View>
-      <ScrollView style={styles.referList}>
-        <View
-          style={{
-            flexDirection: "column",
-            width: "100%",
-            padding: 15,
-          }}
-        >
-          <Text
-            style={{
-              textAlign: "center",
-              fontSize: 20,
-              fontWeight: "bold",
-              color: "#2787b4",
-            }}
-          >
-            Hãy giới thiệu My HealUP tới bạn bè ngay!
-          </Text>
-          <Text
-            style={{
-              textAlign: "center",
-              fontSize: 16,
-              top: 20,
-            }}
-          >
-            Để cùng trải nghiệm dịch vụ tư vấn từ xa và chăm sóc sức khoẻ hiệu
-            quả mọi lúc mọi nơi ngay trên điện thoại
-          </Text>
-        </View>
-        <View style={{ marginTop: 30, width: "100%", height: "20%" }}>
-          <Text style={{ fontWeight: "bold" }}>Gửi mã giới thiệu của bạn</Text>
+      <View style={styles.referList}>
+        <ScrollView style={{ paddingHorizontal: 16 }}>
           <View
             style={{
-              flexDirection: "row",
-              justifyContent: "space-between",
-              height: "60%",
-              alignItems: "center",
-              borderBottomColor: "#797878",
-              borderBottomWidth: 1,
+              flexDirection: "column",
+              width: "100%",
+              padding: 15,
             }}
           >
-            <Text>0332123459988</Text>
-            <TouchableOpacity
+            <Text
               style={{
-                width: "35%",
-                height: "50%",
-                backgroundColor: "#3183c7",
-                justifyContent: "center",
-                alignItems: "center",
-                borderRadius: 5,
+                textAlign: "center",
+                fontSize: 20,
+                fontWeight: "bold",
+                color: "#2787b4",
               }}
             >
-              <Text
+              Hãy giới thiệu My HealUP tới bạn bè ngay!
+            </Text>
+            <Text
+              style={{
+                textAlign: "center",
+                fontSize: 16,
+                top: 20,
+              }}
+            >
+              Để cùng trải nghiệm dịch vụ tư vấn từ xa và chăm sóc sức khoẻ hiệu
+              quả mọi lúc mọi nơi ngay trên điện thoại
+            </Text>
+          </View>
+          <View style={{ marginTop: 30, width: "100%", height: "20%" }}>
+            <Text style={{ fontWeight: "bold" }}>
+              Gửi mã giới thiệu của bạn
+            </Text>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                height: "60%",
+                alignItems: "center",
+                borderBottomColor: "#797878",
+                borderBottomWidth: 1,
+              }}
+            >
+              <Text>0332123459988</Text>
+              <TouchableOpacity
                 style={{
-                  fontWeight: "bold",
-                  color: "white",
+                  width: "35%",
+                  height: "50%",
+                  backgroundColor: "#3183c7",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  borderRadius: 5,
                 }}
               >
-                Sao Chép
+                <Text
+                  style={{
+                    fontWeight: "bold",
+                    color: "white",
+                  }}
+                >
+                  Sao Chép
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={styles.itemRefer}>
+            <View
+              style={{
+                width: "47%",
+                height: "100%",
+                justifyContent: "space-between",
+              }}
+            >
+              <Text style={{ color: "#3183c7", fontWeight: "800" }}>
+                Tải ứng dụng Bệnh nhân
               </Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-        <View style={styles.itemRefer}>
-          <View
-            style={{
-              width: "47%",
-              height: "100%",
-              justifyContent: "space-between",
-            }}
-          >
-            <Text style={{ color: "#3183c7", fontWeight: "800" }}>
-              Tải ứng dụng Bệnh nhân
-            </Text>
-            <Text>
-              Quét mã QR bên cạnh hoặc tìm kiếm từ khoá{" "}
-              <Text style={{ fontWeight: "bold", fontSize: 12.5 }}>
-                My HealUP
-              </Text>{" "}
-              trên CH Play hoặc Apps Store
-            </Text>
-            <TouchableOpacity
+              <Text>
+                Quét mã QR bên cạnh hoặc tìm kiếm từ khoá{" "}
+                <Text style={{ fontWeight: "bold", fontSize: 12.5 }}>
+                  My HealUP
+                </Text>{" "}
+                trên CH Play hoặc Apps Store
+              </Text>
+              <TouchableOpacity
+                style={{
+                  width: "90%",
+                  height: 40,
+                  backgroundColor: "#3183c7",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  borderRadius: 5,
+                }}
+              >
+                <Text style={{ color: "white" }}>Chia sẻ Link</Text>
+              </TouchableOpacity>
+            </View>
+            <View
               style={{
-                width: "90%",
-                height: 40,
-                backgroundColor: "#3183c7",
-                justifyContent: "center",
+                width: "55%",
+                height: "100%",
                 alignItems: "center",
-                borderRadius: 5,
+                justifyContent: "space-evenly",
               }}
             >
-              <Text style={{ color: "white" }}>Chia sẻ Link</Text>
-            </TouchableOpacity>
+              <Image
+                style={{ width: "90%", height: "80%" }}
+                source={require("../../assets/img/content/info.jpg")}
+              />
+              <Text style={{ textDecorationLine: "underline" }}>Phóng to</Text>
+            </View>
           </View>
-          <View
-            style={{
-              width: "55%",
-              height: "100%",
-              alignItems: "center",
-              justifyContent: "space-evenly",
-            }}
-          >
-            <Image
-              style={{ width: "90%", height: "80%" }}
-              source={require("../../assets/img/content/info.jpg")}
-            />
-            <Text style={{ textDecorationLine: "underline" }}>Phóng to</Text>
-          </View>
-        </View>
-        <View style={styles.itemRefer}>
-          <View
-            style={{
-              width: "47%",
-              height: "100%",
-              justifyContent: "space-between",
-            }}
-          >
-            <Text style={{ color: "#3183c7", fontWeight: "800" }}>
-              Tải ứng dụng Bác sĩ
-            </Text>
-            <Text>
-              Quét mã QR bên cạnh hoặc tìm kiếm từ khoá{" "}
-              <Text style={{ fontWeight: "bold", fontSize: 12.5 }}>
-                Our Health Doctor
-              </Text>{" "}
-              trên CH Play hoặc Apps Store
-            </Text>
-            <TouchableOpacity
+          <View style={styles.itemRefer}>
+            <View
               style={{
-                width: "90%",
-                height: 40,
-                backgroundColor: "#3183c7",
-                justifyContent: "center",
-                alignItems: "center",
-                borderRadius: 5,
+                width: "47%",
+                height: "100%",
+                justifyContent: "space-between",
               }}
             >
-              <Text style={{ color: "white" }}>Chia sẻ Link</Text>
-            </TouchableOpacity>
+              <Text style={{ color: "#3183c7", fontWeight: "800" }}>
+                Tải ứng dụng Bác sĩ
+              </Text>
+              <Text>
+                Quét mã QR bên cạnh hoặc tìm kiếm từ khoá{" "}
+                <Text style={{ fontWeight: "bold", fontSize: 12.5 }}>
+                  Our Health Doctor
+                </Text>{" "}
+                trên CH Play hoặc Apps Store
+              </Text>
+              <TouchableOpacity
+                style={{
+                  width: "90%",
+                  height: 40,
+                  backgroundColor: "#3183c7",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  borderRadius: 5,
+                }}
+              >
+                <Text style={{ color: "white" }}>Chia sẻ Link</Text>
+              </TouchableOpacity>
+            </View>
+            <View
+              style={{
+                width: "55%",
+                height: "100%",
+                alignItems: "center",
+                justifyContent: "space-evenly",
+              }}
+            >
+              <Image
+                style={{ width: "90%", height: "80%" }}
+                source={require("../../assets/img/content/info.jpg")}
+              />
+              <Text style={{ textDecorationLine: "underline" }}>Phóng to</Text>
+            </View>
           </View>
-          <View
-            style={{
-              width: "55%",
-              height: "100%",
-              alignItems: "center",
-              justifyContent: "space-evenly",
-            }}
-          >
-            <Image
-              style={{ width: "90%", height: "80%" }}
-              source={require("../../assets/img/content/info.jpg")}
-            />
-            <Text style={{ textDecorationLine: "underline" }}>Phóng to</Text>
-          </View>
-        </View>
-      </ScrollView>
+        </ScrollView>
+      </View>
     </SafeAreaView>
   );
 }
@@ -215,9 +219,6 @@ const styles = StyleSheet.create({
   },
   referList: {
     flex: 1,
-    height: "100%",
-    width: "100%",
-    paddingHorizontal: 16,
     backgroundColor: "#f0f0f0",
   },
   itemRefer: {
